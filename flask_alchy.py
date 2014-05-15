9c29d41d1b2abc0d9ffbcb98b69885d862fca582
Derrick Gilland <dgilland@gmail.com>
2014-05-15 11:31:18 -0400

    Rename FlaskAlchy class to Alchy and PEP8/Pylint compliance.

--- flask_alchy.py
+++ flask_alchy.py
@@ -1,17 +1,22 @@
-'''Integrate alchy with Flask
-'''
+"""Integrate alchy with Flask SQLAlchemy
+"""
 
 __version__ = '0.3.0'
 __author__ = 'Derrick Gilland <dgilland@gmail.com>'
 
 
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 from alchy import make_declarative_base, QueryModel, ManagerMixin
 
 
-class FlaskAlchy(SQLAlchemy, ManagerMixin):
-    def __init__(self, app=None, use_native_unicode=True, session_options=None, Model=None):
+class Alchy(SQLAlchemy, ManagerMixin):
+    """Flask extension that integrates alchy with Flask-SQLAlchemy."""
+    def __init__(self,
+                 app=None,
+                 use_native_unicode=True,
+                 session_options=None,
+                 Model=None):
         if session_options is None:
             session_options = {}
 
@@ -19,14 +24,15 @@ class FlaskAlchy(SQLAlchemy, ManagerMixin):
 
         self.Model = Model
 
-        super(FlaskAlchy, self).__init__(app, use_native_unicode, session_options)
+        super(Alchy, self).__init__(
+            app, use_native_unicode, session_options)
 
         self.Query = session_options['query_cls']
 
     def make_declarative_base(self):
-        '''Override parent function with alchy's'''
+        """Override parent function with alchy's"""
         return make_declarative_base(self.session, Model=self.Model)
 
     def __getattr__(self, attr):
-        '''Delegate all other attributes to self.session'''
+        """Delegate all other attributes to self.session"""
         return getattr(self.session, attr)
--- test_flask_alchy.py
+++ test_flask_alchy.py
@@ -5,12 +5,13 @@ from sqlalchemy import Column, types
 from sqlalchemy.ext.declarative import declarative_base
 
 from flask import Flask
-from flask.ext.alchy import FlaskAlchy
+from flask.ext.alchy import Alchy
 from flask.ext.sqlalchemy import SQLAlchemy, BaseQuery
 
 import alchy
 
-class TestFlaskAlchy(TestCase):
+
+class TestAlchy(TestCase):
 
     def setUp(self):
         self.app = Flask(__name__)
@@ -18,24 +19,26 @@ class TestFlaskAlchy(TestCase):
 
     def test_default_query_class(self):
         """Test that session options default to query_cls=alchy.Query"""
-        db = FlaskAlchy(self.app)
+        db = Alchy(self.app)
 
         self.assertEquals(db.Query, alchy.QueryModel)
         self.assertIsInstance(db.session.query(), alchy.QueryModel)
 
     def test_override_query_class(self):
         """Test that session query class can be overridden"""
-        db = FlaskAlchy(self.app, session_options={'query_cls': BaseQuery})
+        db = Alchy(self.app, session_options={'query_cls': BaseQuery})
 
         self.assertEquals(db.Query, BaseQuery)
         self.assertIsInstance(db.session.query(), BaseQuery)
 
     def test_model_class(self):
         """Test that model class uses alchy.ModelBase"""
-        db = FlaskAlchy(self.app)
+        db = Alchy(self.app)
 
-        self.assertEquals(db.Model.__dict__['__init__'], alchy.model.ModelBase.__init__)
-        self.assertIsInstance(db.Model.__dict__['query'], alchy.query.QueryProperty)
+        self.assertEquals(
+            db.Model.__dict__['__init__'], alchy.model.ModelBase.__init__)
+        self.assertIsInstance(
+            db.Model.__dict__['query'], alchy.query.QueryProperty)
 
     def test_override_model_class(self):
         """Test that base model class can be overridden with custom class"""
@@ -50,13 +53,15 @@ class TestFlaskAlchy(TestCase):
             _id = Column(types.Integer(), primary_key=True)
             name = Column(types.String())
 
-        db = FlaskAlchy(self.app, Model=Model)
+        db = Alchy(self.app, Model=Model)
 
-        self.assertTrue(issubclass(db.Model, MyModelBase), 'db.Model should be a subclass of MyModelBase')
+        self.assertTrue(issubclass(db.Model, MyModelBase),
+                        'db.Model should be a subclass of MyModelBase')
 
         db.create_all()
 
-        self.assertEquals(db.session.query(Foo).all(), Foo.query.all(), 'Model classes should have a query property')
+        self.assertEquals(db.session.query(Foo).all(), Foo.query.all(),
+                          'Model classes should have a query property')
 
         record = Foo(name='Name')
 
@@ -64,5 +69,4 @@ class TestFlaskAlchy(TestCase):
 
     def test_flask_alchy_subclasses(self):
         """Test that flask-alchy subclasses from flask-sqlalchemy and alchy"""
-        self.assertTrue(issubclass(FlaskAlchy, (SQLAlchemy, alchy.ManagerMixin)))
-
+        self.assertTrue(issubclass(Alchy, (SQLAlchemy, alchy.ManagerMixin)))
