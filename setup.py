08ab072810971d44e836cc9219ad57999944acca
Derrick Gilland <dgilland@gmail.com>
2016-03-21 20:39:08 -0400

    Release v0.5.0.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Alchy',
-    version='0.4.0',
+    version='0.5.0',
     url='https://github.com/dgilland/flask-alchy',
     license='MIT',
     author='Derrick Gilland',
@@ -20,8 +20,8 @@ setup(
     long_description=__doc__,
     py_modules=['flask_alchy'],
     install_requires=[
-        'alchy>=0.11.3',
-        'Flask-SQLAlchemy>=1.0'
+        'alchy>=2.2.0',
+        'Flask-SQLAlchemy>=2.1.0'
     ],
     test_suite='test_flask_alchy',
     keywords='flask alchy sqlalchemy databases',
